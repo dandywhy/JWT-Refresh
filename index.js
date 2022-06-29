e1f954ba0ea4d7c831a7ec3494bd7e049a20ca24
Dandy <lavenderflyyaya@gmail.com>
2022-06-29 20:07:34 +0800

    modify

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const whiteList = [process.env.ORIGIN1]
 
 app.use(cors({
   origin: function(origin, callback) {
-    if (whiteList.includes(origin)) {
+    if (!origin || whiteList.includes(origin)) {
       return callback(null, origin)
     }
     return callback('Error cors origin: ' + origin + ' No authorization')
